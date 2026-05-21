7d282efc7b18a4b5f9d1606d02e8f129d9b65602
askkakkera-ui <askkakkera@gmail.com>
2026-05-21 21:16:57 +0530

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,10 +38,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={${geistSans.variable} ${geistMono.variable} h-full antialiased}
-    >
+    <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
+}
