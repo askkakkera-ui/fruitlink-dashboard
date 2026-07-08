a55cc9b8675f034cb4fc99433b7ca461ca8f1c3c
askkakkera-ui <askkakkera@gmail.com>
2026-07-08 03:34:21 +0200

    Update dashboard.tsx

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1893,6 +1893,7 @@ function FleetMapPage({ machines }: { machines: any[] }) {
                     ))}
                   </div>
                 </div>
+              </div>}
               </div>
             )
           })}
